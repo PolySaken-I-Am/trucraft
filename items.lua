a078f9ba738e0d35dce5e2e2ab6a44178f7d7a74
PolySaken <37032857+PolySaken-I-Am@users.noreply.github.com>
2020-01-11 13:58:11 +1300

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,6 +1,7 @@
+local S = minetest.get_translator("trucraft")
 
 minetest.register_craftitem("trucraft:string", {
-	description="Plant Fiber",
+	description = S("Plant Fiber"),
 	inventory_image="poly_tcstring2.png",
 	on_use = function(itemstack, player, pointed_thing)
 		if itemstack:get_count() > 3 then
@@ -44,17 +45,17 @@ minetest.register_item(":", {
 })
 
 minetest.register_craftitem("trucraft:fabric", {
-	description="Grassweave Mat",
+	description = S("Grassweave Mat"),
 	inventory_image="poly_tcstring3.png"
 })
 
 minetest.register_craftitem("trucraft:sieve", {
-	description="Basic Sieve",
+	description = S("Basic Sieve"),
 	inventory_image="poly_tcsieve.png",
 })
 
 minetest.register_craftitem("trucraft:shaft", {
-	description="Basic Wooden Shaft",
+	description = S("Basic Wooden Shaft"),
 	inventory_image="poly_tcshaft.png",
 })
 
@@ -75,92 +76,92 @@ minetest.override_item("default:axe_mese", {
 })
 
 minetest.register_craftitem("trucraft:copper_blade", {
-	description="Crude Copper Blade",
+	description = S("Crude Copper Blade"),
 	inventory_image="poly_tccopperb.png",
 })
 
 minetest.register_craftitem("trucraft:shaft2", {
-	description="Sturdy Wooden Shaft",
+	description = S("Sturdy Wooden Shaft"),
 	inventory_image="poly_tcshaft2.png",
 })
 
 minetest.register_craftitem("trucraft:bronze", {
-	description="Bronze Mix",
+	description = S("Bronze Mix"),
 	inventory_image="poly_tcbronze.png",
 })
 
 minetest.register_craftitem("trucraft:bronze_plate", {
-	description="Bronze Plate",
+	description = S("Bronze Plate"),
 	inventory_image="poly_tcbronzep.png",
 })
 
 minetest.register_craftitem("trucraft:bronze_wedge", {
-	description="Bronze Wedge",
+	description = S("Bronze Wedge"),
 	inventory_image="poly_tcbronzew.png",
 })
 
 minetest.register_craftitem("trucraft:bronze_hook", {
-	description="Bronze Hook-Blade",
+	description = S("Bronze Hook-Blade"),
 	inventory_image="poly_tcbronzeh.png",
 })
 
 minetest.register_craftitem("trucraft:bronze_blade", {
-	description="Bronze Blade",
+	description = S("Bronze Blade"),
 	inventory_image="poly_tcbronzeb.png",
 })
 
 minetest.register_craftitem("trucraft:steel_plate", {
-	description="Steel Plate",
+	description = S("Steel Plate"),
 	inventory_image="poly_tcbronzep.png^[colorize:#bbffff90",
 })
 
 minetest.register_craftitem("trucraft:steel_wedge", {
-	description="Steel Wedge",
+	description = S("Steel Wedge"),
 	inventory_image="poly_tcbronzew.png^[colorize:#bbffff90",
 })
 
 minetest.register_craftitem("trucraft:steel_hook", {
-	description="Steel Hook-Blade",
+	description = S("Steel Hook-Blade"),
 	inventory_image="poly_tcbronzeh.png^[colorize:#bbffff90",
 })
 
 minetest.register_craftitem("trucraft:steel_blade", {
-	description="Steel Blade",
+	description = S("Steel Blade"),
 	inventory_image="poly_tcbronzeb.png^[colorize:#bbffff90",
 })
 
 
 minetest.register_craftitem("trucraft:steel_rod", {
-	description="Steel Rod",
+	description = S("Steel Rod"),
 	inventory_image="default_stick.png^[colorize:#bbffff90",
 })
 
 minetest.register_craftitem("trucraft:grass_lump", {
-	description="Plant Mush",
+	description = S("Plant Mush"),
 	inventory_image="poly_tclump.png",
 })
 
 minetest.register_craftitem("trucraft:glue", {
-	description="Plant Glue",
+	description = S("Plant Glue"),
 	inventory_image="poly_tclump2.png",
 })
 
 minetest.register_craftitem("trucraft:grit", {
-	description="Diamond Grit",
+	description = S("Diamond Grit"),
 	inventory_image="poly_tcdiamondd.png",
 })
 
 minetest.register_craftitem("trucraft:coal_dust", {
-	description="Coal Grit",
+	description = S("Coal Grit"),
 	inventory_image="poly_tccoald.png",
 })
 
 minetest.register_craftitem("trucraft:polymer", {
-	description="Plastic Substrate",
+	description = S("Plastic Substrate"),
 	inventory_image="poly_tclump3.png",
 })
 
 minetest.register_craftitem("trucraft:plastic", {
-	description="Plastic Sheet",
+	description= S("Plastic Sheet"),
 	inventory_image="poly_tclump4.png",
 })
